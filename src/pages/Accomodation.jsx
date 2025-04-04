3fe82db7f505d563d180cfc1295e978207b82580
Joanne <joanne.bazin@gmail.com>
2025-04-04 18:06:05 +0200

    feat: add app router

---
+++ src/pages/Accomodation.jsx
@@ -0,0 +1,9 @@
+const Accomodation = () => {
+  return (
+    <div>
+      <h2>Logement</h2>
+    </div>
+  );
+};
+
+export default Accomodation;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Accueil</h2>
+    </div>
+  );
+};
+
+export default Home;
