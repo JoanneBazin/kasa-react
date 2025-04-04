01baa63470b277ac8c95f2539817a71f81060925
Joanne <joanne.bazin@gmail.com>
2025-04-04 20:00:24 +0200

    app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import AppRouter from "./routes/AppRouter";
+import { BrowserRouter } from "react-router";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main>
         <AppRouter />
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,32 @@
+import { Link, NavLink } from "react-router";
+import logo from "../assets/logo-header.png";
+import "./Header.scss";
+
 const Header = () => {
   return (
-    <div>
-      <h1>Header !</h1>
-    </div>
+    <header className="header">
+      <img src={logo} alt="Logo Kasa" />
+      <nav>
+        <ul className="navbar">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              A propos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
