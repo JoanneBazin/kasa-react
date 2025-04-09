e435330dffb9f6674a5ba4b9e758e2e8ac577933
Joanne <joanne.bazin@gmail.com>
2025-04-09 15:08:01 +0200

    edit 404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router";
+import "../styles/pages/Error.scss";
+
 const Error = () => {
   return (
-    <div>
-      <h2>ERROR 404</h2>
-    </div>
+    <section className="error-container">
+      <h2 className="error-title">404</h2>
+      <p className="error-message">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link className="error-redirection" to="/">
+        Retourner sur la page d'accueil
+      </Link>
+    </section>
   );
 };
 
